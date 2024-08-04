17cb998c6360d153ee501f3c9520cbf7a561ff68
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-08-04 11:00:05 -0400

    Update data.tsx
    Signed-off-by: Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Medha Praneeth Modulla',
+  description: "Site built with React, Next.js and Tailwind CSS",
 };
 
 /**
@@ -69,13 +69,12 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Medha Praneeth Modulla.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a <strong className="text-stone-100">Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">InterSystems </strong> helping build a InterSystems Data Fabric Studio.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
         In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
@@ -108,12 +107,12 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
+    {label: 'Location', text: 'Boston, MA', Icon: MapIcon},
+    {label: 'Age', text: '25', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Indian', Icon: FlagIcon},
     {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Study', text: 'Carnegie Mellon University', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'InterSystems Corporation', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -268,39 +267,64 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
+    date: 'August 2022 - December 2023',
+    location: 'Carnegie Mellon Universtiy',
+    title: 'Master of Information Systems Management',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
+    date: 'August 2016 - May 2020',
+    location: 'Birla Institute of Technology and Science, Pilani',
+    title: 'Bachelor of Engineering in Electronics and Instrumentation',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'March 2024 - Present',
+    location: 'InterSystems Corporation',
+    title: 'Software Engineer, Quality Development',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        I developed end-end tests for InterSystems Data Fabric Studio using Seleniumbase framework in Python.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'May 2023 - August 2023',
+    location: 'Honeywell',
+    title: 'Software Engineer Intern',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        At Honeywell, I was responsible for creating a web page with Angular 6 to enhance emergency access to 
+        building information and developing backend functionalities in C# to manage and store crucial building data in the customer database.
+      </p>
+    ),
+  },
+  {
+    date: 'July 2020 - July 2022',
+    location: 'Intel Corporation',
+    title: 'Software Application Development Engineer',
+    content: (
+      <p>
+        During my time at Intel, I was responsible for creating custom browser-based applications for the assembly and test factories using .NET Framework and WPF. 
+        I designed and implemented PL/SQL stored procedures to enforce business rules and maintain data integrity within the production line systems. I also automated 
+        manual test cases for factory automation software, which significantly reduced testing time. In addition, I oversaw the deployment of factory automation software, 
+        working with stakeholders to minimize downtime during updates. I collaborated with cross-functional teams to streamline project dependencies and expedite delivery. 
+        Furthermore, I provided L3 support by addressing complex technical issues reported by customers, demonstrating strong debugging and problem-solving skills, effectively 
+        minimizing production impact.
+      </p>
+    ),
+  },
+  {
+    date: 'July 2019 - December 2019',
+    location: 'Intel Corporation',
+    title: 'Software Engineer Intern',
+    content: (
+      <p>
+        As an intern at Intel, I spearheaded the migration project for the Recipe Management System from Microsoft 2008 to 2016 servers, within the stipulated timeline with 
+        minimal disruption to operations. I also developed UI functionalities using Windows Presentation Foundation and constructing platform API's in C#.
       </p>
     ),
   },
@@ -340,23 +364,23 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'mmodulla@alumni.cmu.edu',
+      href: 'mailto:mmodulla@alumni.cmu.edu',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Boston MA, United States of America',
+      href: 'https://www.google.com/maps/place/Boston,+MA/@42.3143205,-71.052684,12z/data=!3m1!4b1!4m6!3m5!1s0x89e3652d0d3d311b:0x787cbf240162e8a0!8m2!3d42.3600825!4d-71.0588801!16zL20vMDFjeF8?entry=ttu',
     },
     {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      type: ContactType.Linkedin,
+      text: 'Medha Praneeth Reddy Modulla',
+      href: 'https://www.linkedin.com/in/reddymedhapraneeth/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'praneethmodulla',
+      href: 'https://github.com/praneethmodulla',
     },
   ],
 };
@@ -365,9 +389,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/praneethmodulla'},
+  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/reddymedhapraneeth/'},
+  // {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
+  // {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
 ];
